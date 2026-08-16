60ef81436a420ac0f097c72b4f4a83ec20e51b93
Pedro <projasma2002@gmail.com>
2026-08-15 23:29:49 -0500

    FIX: Agregar consulta de conteo a AccountMovementRepository para los movimientos de la cuenta

--- src/main/java/com/bank/account/repository/AccountMovementRepository.java
+++ src/main/java/com/bank/account/repository/AccountMovementRepository.java
@@ -1,6 +1,7 @@
 package com.bank.account.repository;
 
 import com.bank.account.model.AccountMovement;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -30,6 +31,7 @@ public interface AccountMovementRepository extends ReactiveMongoRepository<Accou
      * @param to        exclusive end
      * @return count
      */
+    @Query(value = "{ 'accountId': ?0, 'occurredAt': { $gte: ?1, $lt: ?2 } }", count = true)
     Mono<Long> countByAccountIdAndOccurredAtGreaterThanEqualAndOccurredAtLessThan(
             String accountId, Instant from, Instant to);
 }
